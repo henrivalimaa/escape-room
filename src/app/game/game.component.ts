e72707ea3fc69cc12ce66b0bf38129e25a52b493
henrivalimaa <valimaa.henri@gmail.com>
2018-07-05 18:08:09 +0300

    Camera bug fixex

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -49,14 +49,7 @@ export class GameComponent implements OnInit, AfterViewChecked {
   handleInputChange(e) {
     var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
 
-    var pattern = /image-*/;
     var reader = new FileReader();
-
-    /*
-    if (!file.type.match(pattern)) {
-      alert('invalid format');
-      return;
-    }*/
 
     this.loaded = false;
 
